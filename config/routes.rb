bc75f8bb48ea82f7b560428baa63d54503d00cb8
Ryan Ricardo <ryanlima08021997@gmail.com>
2018-06-22 20:50:29 -0300

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  #get 'movies/index'
+  get 'movies' => "movies#index"
+  #get 'movies/new'
+  get 'movies/new' => "movies#new"
+  #get 'movies/show'
+  get 'movies/:id' => "movies#show"
+  #get 'movies/create'
+  post 'movies' => "movies#create"
+  #Edit
+  get 'movies/:id/edit' => "movies#edit"
+  #Update
+  put 'movies/:id/' => "movies#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
