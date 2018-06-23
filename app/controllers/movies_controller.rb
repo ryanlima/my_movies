ba5b51117f84f98fb29a79b5a2ab6261fb696829
Ryan Ricardo <ryanlima08021997@gmail.com>
2018-06-22 21:09:09 -0300

    commit dor de cabeça

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update]
+
   def index
     @movies = Movie.all
   end
@@ -11,9 +13,29 @@ class MoviesController < ApplicationController
     render file: "#{Rails.root}/public/404.html", status: 404
   end
 
+  def edit
+    id = params['id']
+    @movie = Movie.find(id)
+  rescue ActiveRecord::RecordNotFound
+    @movie = nil
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
+  def update
+    @movie.update(movie_params)
+    if @movie.save
+      redirect_to action: :show, id: @movie.id
+    else
+      render :edit, id: @movie.id
+    end
+  #rescue ActiveRecord::RecordNotFound
+   # render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
   def new
     @movie = Movie.new
   end
+ 
 
   def create
     @movie = Movie.new(movie_params)
@@ -24,31 +46,17 @@ class MoviesController < ApplicationController
     end
   end
 
-  def edit
-    id = params['id']
-    @movie = Movie.find(id)
-  rescue ActiveRecord::RecordNotFound
-    @movie = nil
-    render file: "#{Rails.root}/public/404.html", status: 404
-  end
-
-  def update
-    id = params['id']
-    @movie = Movie.find(id)
-    @movie.update(movie_params)
-    if @movie.save
-      redirect_to action: :show, id: @movie.id
-    else
-      render :edit, id: @movie.id
-    end
-  rescue ActiveRecord::RecordNotFound
-    render file: "#{Rails.root}/public/404.html", status: 404
-  end
-
   private
 
   def movie_params
     params.require(:movie).permit(:title, :release_date, :description)
+  end
+
+  def set_movie
+    id = params['id']
+    @movie = Movie.find(id)
+  rescue ActiveRecord::RecordNotFound
+    render file: "#{Rails.root}/public/404.html", status: 404
   end
 
 end
